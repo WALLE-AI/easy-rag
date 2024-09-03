f4db2e05ad0b28fb956e54bb37383606817b4b0c
WALL-AI <gaojing850063636@gmail.com>
2024-09-03 15:52:07 +0800

    feat:add download images function

--- test/test_preprocess_images.py
+++ test/test_preprocess_images.py
@@ -2,10 +2,11 @@ import json
 
 import loguru
 import pandas as pd
+import requests
 
 from rag.entities.entity_images import ImageTableProcess
 
-sheet_name = ["Sheet"]
+sheet_name = ["Sheet1","Sheet1(2)","Sheet1(3),Sheet1(4),Sheet1(5)"]
 
 
 ##{'单号': '0000096ed25ec73466f0bbc48061acc4', '项目id': 232, '照片': 'https://zhgd-prod-oss.oss-cn-shenzhen.aliyuncs.com/af0d013d-2f52-2c97-413b-55970976ec61.jpg', '隐患部位': 'S3首层', '标准隐患编号': 'dba507c450c247da989bc154c0f80612', '隐患内容': '构造柱钢筋未满绑', 'company_hidden_id': 'dba507c450c247da989bc154c0f80612', 'name': '建筑装饰装修'}
@@ -18,14 +19,33 @@ def write_json_file(data_dict,save_file_name):
         file.write(jsonn_str_data)
 
 
+def download_image(url, filename):
+    images_dir_path = "data\\images\\" + filename
+    loguru.logger.info(f"images dir path {images_dir_path}")
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # 检查请求是否成功
+        with open(images_dir_path, 'wb') as f:
+            f.write(response.content)
+            loguru.logger.info(f"image save：{filename}")
+    except requests.RequestException as e:
+        loguru.logger.info(f"request error：{e}")
+    except IOError as e:
+        loguru.logger.info(f"request io error：{e}")
+
+
 def read_xlsx_file_to_save_json(file_path):
     data = pd.read_excel(file_path)
+    data = data.head(10)
     data_save_list = []
     for index, row_data in data.iterrows():
         data_dict = row_data.to_dict()
+        image_name = data_dict['照片'].split("https://zhgd-prod-oss.oss-cn-shenzhen.aliyuncs.com/")[-1]
+        ##download image dir
+        download_image(data_dict['照片'],image_name)
         data_entity = ImageTableProcess(
             id=str(index),
-            image_id=data_dict['照片'].split("https://zhgd-prod-oss.oss-cn-shenzhen.aliyuncs.com/")[-1],
+            image_id=image_name,
             image_correct_id="",
             image_oss_url=data_dict['照片'],
             conversations=[{}],
@@ -39,12 +59,11 @@ def read_xlsx_file_to_save_json(file_path):
             label="0"
         )
         data_save_list.append(data_entity.to_dict())
-        if not index % 500 and index !=0:
+        if not index % 5 and index !=0:
             save_file_name = "data/images_table_format_"+str(index) +".json"
             write_json_file(data_save_list,save_file_name)
             ##每超过10W，保存一次，清洗之前的数组中数据
             data_save_list.clear()
-
 
 
 def preprocee_table_images_data():
