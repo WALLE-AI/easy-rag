c0f091183e3220f4ce6012e9c7d80056d2bc71fb
WALLE-AI <gaojing850063636@gmail.com>
2024-09-08 21:18:05 +0800

    feat: test xinference client embedding and reranker

--- test/test_main.py
+++ test/test_main.py
@@ -6,6 +6,7 @@ from test_vllm_openai import test_vllm_openai_client
 # from test.test_node_plan import test_node_plan
 # from test.test_openrouter import test_invoke_model, test_invoke_stream_model, test_invoke_image_model
 from test_preprocess_images import preprocee_table_images_data
+from test_xinference_client import test_invoke_model_xinference, test_invoke_model_reranker_xinference
 
 # from test.test_reranker import test_reranker_model
 # from test.test_vision_model_qs import write_file, llm_execute
@@ -14,4 +15,4 @@ if __name__ =="__main__":
     loguru.logger.info(f"test start")
     # test_provider_credentials_validate()
     # test_get_models()
-    preprocee_table_images_data()
+    test_invoke_model_xinference()
